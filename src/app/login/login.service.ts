5a37f60974845256e7d5705fb575d297654103aa
user135790 <santiagoramirezperez15@gmail.com>
2025-01-23 16:35:57 -0500

    configuracion para uso de peticiones y creacion servicio de login

--- src/app/login/login.service.ts
+++ src/app/login/login.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  private apiUrl = "http://yonoseque.com"
+  constructor(private http:HttpClient) { }
+
+  sendLogin(data:any): Observable<any>{
+    return this.http.post(this.apiUrl+"/login", data)
+  }
 }
