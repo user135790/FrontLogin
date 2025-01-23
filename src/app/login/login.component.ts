f13a945a979dea2769bb7e3d547581446043f8fb
user135790 <santiagoramirezperez15@gmail.com>
2025-01-23 16:34:12 -0500

    Estrutura principal de login, creacion del formulario y validacion, creacion del servicio para enviar la peticion

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,27 @@
 import { Component } from '@angular/core';
+import {ReactiveFormsModule, Validators} from '@angular/forms';
+import {FormControl, FormGroup} from '@angular/forms';
+import { LoginService } from './login.service';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
+  constructor (private service:LoginService){}
+
+  login = new FormGroup({  
+    username: new FormControl('',[Validators.required]),
+    password: new FormControl('',[Validators.required, Validators.minLength(7)])
+  });
+
+
+  onSubmitLogin() {
+
+  }
+  
 }
