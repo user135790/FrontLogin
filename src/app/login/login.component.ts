9653c9c415a20feedfe2a5e12a122d43c646106c
user135790 <santiagoramirezperez15@gmail.com>
2025-01-30 21:15:21 -0500

    Segunda iteracion de estilos en el login con PrimeNG

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,8 @@ import {ReactiveFormsModule, Validators} from '@angular/forms';
 import {FormControl, FormGroup} from '@angular/forms';
 import { UserService } from '../user/user-service.service';
 import { CustomPrimengModule } from '../custom-primeng/custom-primeng.module';
+import { distinctUntilChanged } from 'rxjs';
+
 
 @Component({
   selector: 'app-login',
@@ -10,18 +12,41 @@ import { CustomPrimengModule } from '../custom-primeng/custom-primeng.module';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent{
 
-  constructor (private service:UserService){}
+  login: FormGroup;
 
-  login = new FormGroup({  
-    nombre: new FormControl('',[Validators.required]),
-    contrasena: new FormControl('',[Validators.required, Validators.minLength(7)])
-  });
+  constructor (private service:UserService){
+    this.login = new FormGroup({  
+      nombre: new FormControl('',[Validators.required]),
+      contrasena: new FormControl('',[Validators.required, Validators.minLength(7)])
+    });
+  }
 
+  get nombre():FormControl{
+    return this.login.get("nombre") as FormControl;
+  }
+
+  get contrasena():FormControl{
+    return this.login.get("contrasena") as FormControl;
+  }
+
+  propiedadesCss = {colorIcono:"#da0e0e", disabled:true}
 
   onSubmitLogin() {
     this.service.sendLogin(this.login.value).subscribe()
   }
+
+  ngOnInit(){
+    this.login.statusChanges.pipe(
+      distinctUntilChanged()).subscribe(estado => {
+        if (estado == "VALID"){
+          this.propiedadesCss.colorIcono = "#15c475"
+          this.propiedadesCss.disabled = false
+        }
+      })
+  }
+
+  
   
 }
